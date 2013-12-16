3fc5ce58c387ef0020fd59c2627589079e5532bd
Miguel Astor <sonofgrendel@gmail.com>
2013-12-16 16:02:29 -0430

    Started programming app states. Incorporated Artemis.

---
+++ src/ve/ucv/ciens/ccg/nxtar/components/VideoFrame.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2013 Miguel Angel Astor Romero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ve.ucv.ciens.ccg.nxtar.components;
+
+import com.artemis.Component;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+
+public class VideoFrame extends Component {
+	private Texture frame;
+
+	public VideoFrame(){ }
+
+	public VideoFrame(byte[] imageData){
+		frame = new Texture(new Pixmap(imageData, 0, imageData.length));
+	}
+
+	public Texture getFrame(){
+		return frame;
+	}
+
+	public void setFrame(byte[] imageData){
+		if(frame != null)
+			frame.dispose();
+		frame = new Texture(new Pixmap(imageData, 0, imageData.length));
+	}
+}
---
+++ src/ve/ucv/ciens/ccg/nxtar/states/DummyState.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2013 Miguel Angel Astor Romero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ve.ucv.ciens.ccg.nxtar.states;
+
+/**
+ * Empty state.
+ * 
+ * Completely empty state for debugging purposes.
+ * 
+ * @author miky
+ */
+public class DummyState implements NxtARState{
+
+	@Override
+	public void input(){ }
+
+	@Override
+	public void update(){ }
+
+	@Override
+	public void render(){ }
+
+	@Override
+	public void pause(){ }
+
+	@Override
+	public void resume(){ }
+
+	@Override
+	public void dispose(){ }
+
+}
